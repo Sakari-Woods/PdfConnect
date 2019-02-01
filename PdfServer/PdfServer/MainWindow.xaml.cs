fa7576737603adad57c598434767035e68b3ea5a
Sakari Woods <sakariwoods@gmail.com>
2019-02-01 15:48:58 -0800

    Created initial WPF applications of both server and client, connected TcpSockets for networking, and established initial server multithreading.

---
+++ PdfServer/PdfServer/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PdfServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Defines the thread variables so we can reference our server from
+        // other methods inside this file.
+        serverManage servM = new serverManage();
+        Thread serverThread = null;
+
+        public MainWindow()
+        {
+            // Makes the graphical window display. We leave just this in here,
+            // if we add anything else, the order of running functions gets offset and
+            // we lose all graphical capabilities.
+            InitializeComponent();
+        }
+
+        private void Server_Start(object sender, RoutedEventArgs e)
+        {
+            // Starts a new thread (which is a a class of serverManage.cs) and then calls it's Start() method.
+            serverThread = new Thread(servM.Start);
+
+            // Sets a toggle-boolean so we can start/stop the server later.
+            serverManage.online = true;
+            serverThread.Start();
+            Console.WriteLine("Starting server...");
+
+            // Makes the box green for visual status that the server is running.
+            ServerStatusIndicator.Value = 100;
+        }
+
+        private void Server_Stop(object sender, RoutedEventArgs e)
+        {
+            
+            Console.WriteLine("Stopping server...");
+            try
+            {
+                ServerStatusIndicator.Value = 0;
+
+                // We set the boolean online value to false so it stops looping infinitely, and then call it's Stop method.
+                serverManage.online = false;
+                serverManage.server.Stop();
+                
+                Console.WriteLine("Server stopped.");
+            }
+            catch(Exception a)
+            {
+                Console.WriteLine("Abort failed.");
+            }
+        }
+    }
+}
---
+++ PdfServer/PdfServer/serverManage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PdfServer
+{
+    public class serverManage
+    {
+        // The TcpListener is made public so we can access it from our MainWindow,
+        // this way we can issue commands like serverManage.Start() or serverManage.Stop()
+        public static TcpListener server;
+        // Same goes for our boolean online value, otherwise the only way we can stop the thread is by
+        // forcefully aborting it via serverThread.Abort(), and that's very messy and usually will crash.
+        public static Boolean online = true;
+        public void Start()
+        {
+            Console.WriteLine("Attempting to register TCPListener");
+            // Hooks up to the 5709 port using it's own address(localhost).
+            server = new TcpListener(IPAddress.Any, 5709);
+
+            try
+            {
+                // Starts up the server.
+                server.Start();
+            }
+            catch(Exception b)
+            {
+                Console.WriteLine("Server must already be running, proceeding...");
+            }
+            Console.WriteLine("Server started and running.");
+
+            while (online==true)
+            {
+                // Enables the server to listen and accept connections from a client.
+                try
+                {
+                    // NOTE: The program will wait here until a client connects, once they do it'll proceed to run down the additional commands.
+                    // This is where we need to apply multithreading so each client gets their own thread, and as a result, we can have more than
+                    // one connection at a time.
+                    TcpClient client = server.AcceptTcpClient();
+
+                    // Gets the stream from the client, which should be a handshake
+                    // of who is connecting and what is going to be sent.
+                    NetworkStream netstream = client.GetStream();
+
+                    while (client.Connected)
+                    {
+                        // Creates an empty byte array, basically we need to allocate the space of the incoming message, before we download the message.
+                        // The car needs a garage before it can pull in!
+                        byte[] msg = new byte[32];
+
+                        try
+                        {
+                            // Do a bit of converting from byte -> String.
+                            netstream.Read(msg, 0, msg.Length);
+                            string received = Encoding.Default.GetString(msg).Trim();
+                            received = received.Substring(0, 32);
+                            Console.WriteLine("Received: " + received);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Unknown failure during packet receive.");
+                        }
+                    }
+                }
+                catch(SocketException a)
+                {
+                    Console.WriteLine("Service down");
+                }
+            }           
+        }       
+    }
+}
